339e506193e628f0bc26115ff1d39893ce86f163
jigarthakkar123 <jigar.thakkar.tops@gmail.com>
2024-06-11 18:42:20 +0530

    11-6-24

--- Project/myapp/views.py
+++ Project/myapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import User
+import requests
+import random
 
 # Create your views here.
 def index(request):
@@ -9,3 +12,175 @@ def contact(request):
 
 def product(request):
 	return render(request,'product.html')
+
+def about(request):
+	return render(request,'about.html')
+
+def signup(request):
+	if request.method=="POST":
+		try:
+			User.objects.get(email=request.POST['email'])
+			msg="Email Already Registered"
+			return render(request,'login.html',{'msg':msg})
+		except:
+			if request.POST['password']==request.POST['cpassword']:
+				User.objects.create(
+						fname=request.POST['fname'],
+						lname=request.POST['lname'],
+						email=request.POST['email'],
+						mobile=request.POST['mobile'],
+						address=request.POST['address'],
+						password=request.POST['password'],
+						profile_picture=request.FILES['profile_picture'],
+						usertype=request.POST['usertype']
+					)
+				msg="User Registered Successfully"
+				return render(request,'login.html',{'msg':msg})
+			else:
+				msg="Password & Confirl Password Does Not Matched"
+				return render(request,'signup.html',{'msg':msg})
+	else:
+		return render(request,'signup.html')
+
+def login(request):
+	if request.method=="POST":
+		try:
+			user=User.objects.get(email=request.POST['email'])
+			if user.password==request.POST['password']:
+				request.session['email']=user.email
+				request.session['fname']=user.fname
+				request.session['profile_picture']=user.profile_picture.url
+				if user.usertype=="buyer":
+					return render(request,'index.html')
+				else:
+					return render(request,'seller-index.html')
+			else:
+				msg="Incorrect Password"
+				return render(request,'login.html',{'msg':msg})
+		except:
+			msg="Email Not Registered"
+			return render(request,'login.html',{'msg':msg})
+	else:
+		return render(request,'login.html')
+
+def logout(request):
+	try:
+		del request.session['email']
+		del request.session['fname']
+		del request.session['profile_picture']
+		msg="User Logged Out Successfully"
+		return render(request,'login.html',{'msg':msg})
+	except:
+		msg="User Logged Out Successfully"
+		return render(request,'login.html',{'msg':msg})
+
+def profile(request):
+	user=User.objects.get(email=request.session['email'])
+	if request.method=="POST":
+		user.fname=request.POST['fname']
+		user.lname=request.POST['lname']
+		user.mobile=request.POST['mobile']
+		user.address=request.POST['address']
+		try:
+			user.profile_picture=request.FILES['profile_picture']
+		except:
+			pass
+		user.save()
+		request.session['profile_picture']=user.profile_picture.url
+		msg="Profile Updated Successfully"
+		if user.usertype=="buyer":
+			return render(request,'profile.html',{'user':user,'msg':msg})
+		else:
+			return render(request,'seller-profile.html',{'user':user,'msg':msg})
+	else:
+		if user.usertype=="buyer":
+			return render(request,'profile.html',{'user':user})
+		else:
+			return render(request,'seller-profile.html',{'user':user})
+
+def change_password(request):
+	user=User.objects.get(email=request.session['email'])
+	if request.method=="POST":
+		if user.password==request.POST['old_password']:
+			if request.POST['new_password']==request.POST['cnew_password']:
+				if request.POST['old_password']!=request.POST['new_password']:
+					user.password=request.POST['new_password']
+					user.save()
+					del request.session['email']
+					del request.session['fname']
+					del request.session['profile_picture']
+					msg="Password Changed Successfully"
+					return render(request,'login.html',{'msg':msg})
+				else:
+					msg="Old Password & New Password Can't Be Same"
+					if user.usertype=="buyer":
+						return render(request,'change-password.html',{'msg':msg})
+					else:
+						return render(request,'seller-change-password.html',{'msg':msg})
+			else:
+				msg="New Password & Confirm New Password Does Not Matched"
+				if user.usertype=="buyer":
+					return render(request,'change-password.html',{'msg':msg})
+				else:
+					return render(request,'seller-change-password.html',{'msg':msg})
+		else:
+			msg="Old Password Does Not Matched"
+			if user.usertype=="buyer":
+				return render(request,'change-password.html',{'msg':msg})
+			else:
+				return render(request,'seller-change-password.html',{'msg':msg})
+	else:
+		if user.usertype=="buyer":
+			return render(request,'change-password.html')
+		else:
+			return render(request,'seller-change-password.html')
+
+def forgot_password(request):
+	if request.method=="POST":
+		try:
+			user=User.objects.get(mobile=request.POST['mobile'])
+			mobile=request.POST['mobile']
+			otp=str(random.randint(1000,9999))
+			# url = "https://www.fast2sms.com/dev/bulkV2"
+			# querystring = {"authorization":"DwF5Auzh16qo3fXC2JMSTcOiyBEZmWH0eR8GIg4NbQrpUnKsjvhz0YwyOCGvHJEFuXRrTc7feDVaM1NA","variables_values":otp,"route":"otp","numbers":mobile}
+			# headers = {'cache-control': "no-cache"}
+			# response = requests.request("GET", url, headers=headers, params=querystring)
+			# print(response.text)
+
+			url = "https://www.fast2sms.com/dev/bulkV2"
+			querystring = {"authorization":"DwF5Auzh16qo3fXC2JMSTcOiyBEZmWH0eR8GIg4NbQrpUnKsjvhz0YwyOCGvHJEFuXRrTc7feDVaM1NA","message":"OTP "+otp,"language":"english","route":"q","numbers":mobile}
+			headers = {'cache-control': "no-cache"}
+			response = requests.request("GET", url, headers=headers, params=querystring)
+			print(response.text)
+			print("OTP  : ",otp)
+			request.session['otp']=otp
+			request.session['mobile']=mobile
+			return render(request,'otp.html')			
+		except:
+			msg="Mobile Number Not Registered"
+			return render(request,'forgot-password.html',{'msg':msg})
+	else:
+		return render(request,'forgot-password.html')
+
+def verify_otp(request):
+	otp1=int(request.POST['otp'])
+	otp2=int(request.session['otp'])
+
+	if otp1==otp2:
+		del request.session['otp']
+		return render(request,'new-password.html')
+	else:
+		msg="Invalid OTP"
+		return render(request,'otp.html',{'msg':msg})
+
+def new_password(request):
+	if request.POST['new_password']==request.POST['cnew_password']:
+		user=User.objects.get(mobile=request.session['mobile'])
+		user.password=request.POST['new_password']
+		del request.session['mobile']
+		user.save()
+		msg='password Updated Successfully'
+		return render(request,'login.html',{'msg':msg})
+	else:
+		msg="new password and confirm password doesn't Matched"
+		return render(request,'new-password.html',{'msg':msg})
